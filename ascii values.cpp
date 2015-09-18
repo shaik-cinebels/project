5fac6ad78c6ae257d57fa2f6b56a049f71206ad8
shaik-cinebels <shaikshavalimarrimanu@gmail.com>
2015-09-18 15:21:57 +0530

    dding files

---
+++ ascii values.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+{
+	int i;
+	for(i=-128;i<127;i++)
+	{
+		printf("%d=%c\t",i,i);
+	}
+}
---
+++ pointers.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void fun( int );
+main()
+{
+	int n;
+	scanf("%d",&n);
+	fun(n);
+	return 0;
+}
+ void fun(int n)
+{
+	static int N=n;
+	fun(--N);
+	printf("%d",N);
+}
+
